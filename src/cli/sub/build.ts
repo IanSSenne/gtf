6cadb392657e4d4460ca53ff570033118ba02ce3
Ian Senne (FetchBot) <ianssenne@gmail.com>
2022-04-14 23:46:54 -0700

    update gtf

--- src/cli/sub/build.ts
+++ src/cli/sub/build.ts
@@ -4,17 +4,20 @@ import esbuild from "esbuild";
 import { buildBarrelFile } from "../buildBarrelFile";
 import { getSharedBuildOptions } from "../getSharedBuildOptions";
 export default async function execute(opts: CommandOptions["build"]) {
-  console.log({ opts });
   const entrypoint = buildBarrelFile();
-  esbuild.build({
-    entryPoints: [entrypoint],
-    outfile: path.resolve(process.cwd(), "scripts", "pack.js"),
-    bundle: true,
-    minify: true,
-    format: "esm",
-    sourcemap: "external",
-    external: ["mojang-minecraft", "mojang-gametest", "mojang-minecraft-ui"],
-    treeShaking: true,
-    ...getSharedBuildOptions(),
-  });
+  esbuild
+    .build({
+      entryPoints: [entrypoint],
+      outfile: path.resolve(process.cwd(), "scripts", "pack.js"),
+      bundle: true,
+      minify: true,
+      format: "esm",
+      sourcemap: "external",
+      external: ["mojang-minecraft", "mojang-gametest", "mojang-minecraft-ui"],
+      treeShaking: true,
+      ...getSharedBuildOptions(true),
+    })
+    .then(() => {
+      console.log("Build complete");
+    });
 }
